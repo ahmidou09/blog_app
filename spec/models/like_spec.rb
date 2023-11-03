811707b003fafbcb00168e8d5946e4301a2bffcd
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-03 12:41:16 +0100

    Write RSpec specs for like  model validations

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:post) }
+    it { should belong_to(:user) }
+  end
+
+  describe 'Callbacks' do
+    it 'updates post likes counter after save' do
+      post = create(:post)
+      like = build(:like, post:)
+
+      expect { like.save }.to change { post.reload.likes_counter }.by(1)
+    end
+  end
 end
