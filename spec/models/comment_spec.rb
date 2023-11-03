89864b84af10277d8294a072be1449f0feca9e3f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-03 12:41:55 +0100

    Write RSpec specs for comment model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:post) }
+    it { should belong_to(:user) }
+  end
+
+  describe 'Callbacks' do
+    it 'updates post comments counter after save' do
+      post = create(:post)
+      comment = build(:comment, post:)
+
+      expect { comment.save }.to change { post.reload.comments_counter }.by(1)
+    end
+  end
 end
