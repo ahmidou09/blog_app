ebe86749bacf04ab18545be1ea6be17638ef205d
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-13 23:38:04 +0100

    Implemented pots  with his Route, Controller and View

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   get "/users", to: "users#index", as: "users"
   get "/users/:id", to:"users#show", as: "user"
+  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  get "/users/:user_id/posts/:id", to:"posts#show", as: "post"
 end
