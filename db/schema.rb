799c17349209b3f21b1117368c7459ff9c040ac7
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-13 20:55:33 +0100

    Update schema.rb and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_01_210331) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_02_124909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
 
-  create_table "buyers", id: :integer, default: nil, force: :cascade do |t|
-    t.string "name", limit: 20, null: false
-    t.integer "age", null: false
-    t.string "address", limit: 25
-    t.decimal "salary", precision: 18, scale: 2
-  end
-
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
+    t.integer "author_id", null: false
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +49,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_01_210331) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
