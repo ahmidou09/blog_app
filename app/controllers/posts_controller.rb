73bbb64044941e939cf2bf773feaf9c5540ceb0d
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-23 20:58:38 +0100

    Update PostsController with destroy action, including authorization and counter updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :find_user, only: %i[index show]
+  before_action :find_user, only: %i[index show new create]
+  before_action :find_post, only: %i[show destroy]
 
   def index
     @posts = @user.posts.includes(:comments)
@@ -21,15 +22,33 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      redirect_to user_posts_path(current_user)
+      current_user.update(post_counter: current_user.posts.count)
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    authorize! :destroy, @post
+
+    @user = @post.user
+    @post.comments.destroy_all
+    @post.likes.destroy_all
+    @user.decrement!(:post_counter)
+    @post.destroy
+
+    redirect_to user_posts_path(@user), notice: 'Post deleted successfully.'
+  end
+
+
   private
 
   def post_params
     params.require(:post).permit(:title, :text)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
