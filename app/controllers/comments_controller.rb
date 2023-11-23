03cc89bb366be4a8dd4b462a1c94f4e62fe4e8e8
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-23 20:59:46 +0100

    Update CommentsController with destroy action, including authorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :find_user, only: %i[new create]
-  before_action :find_post, only: %i[new create]
+  before_action :find_user_and_post, only: %i[new create]
+  before_action :find_comment, only: [:destroy]
 
   def new
     @comment = @post.comments.new
@@ -17,17 +17,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    authorize! :destroy, @comment
+
+    @user = @comment.post.user
+    @post = @comment.post
+
+    @comment.destroy
+    redirect_to user_post_path(@user, @post), notice: 'Comment deleted successfully.'
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:text)
   end
 
-  def find_user
+  def find_user_and_post
     @user = current_user
+    @post = Post.find(params[:post_id])
   end
 
-  def find_post
-    @post = Post.find(params[:post_id])
+  def find_comment
+    @comment = Comment.find(params[:id])
   end
 end
