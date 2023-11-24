be258c817a5705b94ef8e1f2a3bbcd6d0b27e761
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-11-24 21:54:39 +0100

    Update session configuration, fix autoload_paths typo, and modify routes for API

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,11 @@ module BlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    # Add this for session configuration
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_blog_app_session'
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Fix the typo in autoload_paths
+    config.autoload_paths << Rails.root.join("lib")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  root "users#index"
 
   resources :users, only: [:index, :show] do
      resources :posts, only: [:index, :show, :new, :create, :destroy] do 
@@ -13,5 +15,12 @@ Rails.application.routes.draw do
      end
    end
 
-  root "users#index"
+   namespace :api, defaults: { format: :json } do
+    resources :sessions
+    resources :users, only: [:index] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
 end
